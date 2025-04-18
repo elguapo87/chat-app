b7a1060e8473bb7ad38cfce250e81dcd2c450bf8
elguapo87 <pglogovac@gmail.com>
2025-04-18 17:50:21 +0200

    Add React toastify container in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Chat from "./pages/chat/Chat";
 import ProfileUpdate from "./pages/profileUpdate/ProfileUpdate";
+import { ToastContainer, toast } from 'react-toastify';           
+import 'react-toastify/dist/ReactToastify.css';                    
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profileUpdate" element={<ProfileUpdate />} />
-      </Routes>
+      <ToastContainer />             
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/profileUpdate" element={<ProfileUpdate />} />
+        </Routes>
     </>
   )
 }
