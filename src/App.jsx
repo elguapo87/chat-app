1cb8e04b7deb264fa7113eab2299b8e1512a1086
elguapo87 <pglogovac@gmail.com>
2025-04-18 18:04:43 +0200

    Create Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Chat from "./pages/chat/Chat";
 import ProfileUpdate from "./pages/profileUpdate/ProfileUpdate";
-import { ToastContainer, toast } from 'react-toastify';           
-import 'react-toastify/dist/ReactToastify.css';                    
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useContext, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+import { AppContext } from "./context/AppContext";
 
 const App = () => {
+
+  const navigate = useNavigate();
+  const { loadUserData } = useContext(AppContext);              
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate("/chat");
+        await loadUserData(user.uid);                      
+    
+      } else {
+        navigate("/");
+      }
+    });
+  }, []);
+
   return (
     <>
-      <ToastContainer />             
+      <ToastContainer />
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/chat" element={<Chat />} />
